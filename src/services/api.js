2ae0c1f97f1dc97e65d85199f54db8428ef8e8cd
kush1214-a <kushkumar4510@gmail.com>
2026-02-02 19:39:08 +0530

    fix api url & players fetch

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-export const getPlayers = async (page = 1) => {
-  const res = await API.get(`/players?page=${page}`);
-  return res.data; // 👈 poora object
-};
+export default api;

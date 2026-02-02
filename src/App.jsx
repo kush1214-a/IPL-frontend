c5afca4af2f53132c26b92aab9aaaa91d01b827c
kush1214-a <kushkumar4510@gmail.com>
2026-02-02 22:17:38 +0530

    fix: export default App for Vite build

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-const App = () => {
-  return (
-    <>
-      {/* ❌ TEMP COMMENT NAVBAR */}
-      {/* <Navbar /> */}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Players from "./pages/Players";
+import Teams from "./pages/Teams";
+import Compare from "./pages/Compare";
 
+function App() {
+  return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/players" element={<Players />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/compare" element={<Compare />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
-};
+}
+
+export default App;   // ✅ THIS LINE IS MANDATORY
